a0127c72f551a64caf83582e7c0964d0739e9fdf
Codi-K <129970419+Codi-K@users.noreply.github.com>
2023-09-13 12:22:55 +0200

    fixed cors

--- api/index.js
+++ api/index.js
@@ -7,15 +7,15 @@ const errorHandling = require("./middleware/ErrorHandling");
 const port = +process.env.PORT || 3000;
 
 // Middleware - Application Level
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Methods", "*");
-//   res.header("Access-Control-Request-Methods", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   res.header("Access-Control-Expose-Headers", "Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Request-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization");
+  next();
+});
 
 // cookieParser & router
 
